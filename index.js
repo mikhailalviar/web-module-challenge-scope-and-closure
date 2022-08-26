4f07da37d90de7ee6000edc2d3cb2e440da09d8f
Mikhail Alviar <mikhailalviar@Mikhails-MBP.attlocal.net>
2022-08-26 14:23:40 -0500

    Completed MVP Portion

--- index.js
+++ index.js
@@ -30,11 +30,12 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  //counter1 code contains a closure while counter2 code does not.
   2. Which of the two uses a closure? How can you tell?
-  
+  //counter1 code uses a closure because the return function gives it access to the outer functions scope
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     //You would use counter1 code if you wan to keep a memory of past numbers
 */
 
 // counter1 code
@@ -64,10 +65,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * Math.floor(3));
 }
 
+console.log('task 2:', (inning()));
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -83,10 +85,20 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(inning, number){
+let homescore = 0;
+let awayscore = 0;
+for (let i=0; i < number; i++){
+  homescore = homescore + inning();
+  awayscore = awayscore + inning();
+}
+  return{
+    Home: homescore,
+    Away: awayscore
+  }
 }
 
+console.log("task 3:", finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -101,11 +113,14 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB, ) {
+  return{
+    Home: inningCB(),
+    Away: inningCB()
+  }
 
 }
-
+console.log("task 4:", (getInningScore(inning)));
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
